b0527302ce55b4583fdfe06247ac254d4910ef28
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-02 11:33:06 +0100

    reenabled unit tests

--- source/Jobbr.WebApi.Tests/ServerRegistrationTests.cs
+++ source/Jobbr.WebApi.Tests/ServerRegistrationTests.cs
@@ -1,122 +1,122 @@
-﻿//using System.Net;
-//using System.Net.Http;
-//using System.Net.Sockets;
-//using Jobbr.Server;
-//using Jobbr.Server.Builder;
-//using Jobbr.Server.WebAPI;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using Jobbr.Server;
+using Jobbr.Server.Builder;
+using Jobbr.Server.WebAPI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-//namespace Jobbr.WebApi.Tests
-//{
-//    [TestClass]
-//    public class ServerRegistrationTests
-//    {
-//        private static string confBackendAddress;
+namespace Jobbr.WebApi.Tests
+{
+    [TestClass]
+    public class ServerRegistrationTests
+    {
+        private static string confBackendAddress;
 
-//        private static JobbrServer GivenARunningServerWithWebApi()
-//        {
-//            var builder = new JobbrBuilder();
+        private static JobbrServer GivenARunningServerWithWebApi()
+        {
+            var builder = new JobbrBuilder();
 
-//            var nextTcpPort = NextFreeTcpPort();
-//            confBackendAddress = $"http://localhost:{nextTcpPort}";
+            var nextTcpPort = NextFreeTcpPort();
+            confBackendAddress = $"http://localhost:{nextTcpPort}";
 
-//            builder.AddWebApi(conf =>
-//            {
-//                conf.BackendAddress = confBackendAddress;
-//            });
+            builder.AddWebApi(conf =>
+            {
+                conf.BackendAddress = confBackendAddress;
+            });
 
-//            var server = builder.Create();
+            var server = builder.Create();
 
-//            server.Start();
+            server.Start();
 
-//            return server;
-//        }
+            return server;
+        }
 
-//        private static int NextFreeTcpPort()
-//        {
-//            var l = new TcpListener(IPAddress.Loopback, 0);
-//            l.Start();
-//            var port = ((IPEndPoint)l.LocalEndpoint).Port;
-//            l.Stop();
-//            return port;
-//        }
+        private static int NextFreeTcpPort()
+        {
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            var port = ((IPEndPoint)l.LocalEndpoint).Port;
+            l.Stop();
+            return port;
+        }
 
-//        private static string CreateUrl(string path)
-//        {
-//            return $"{confBackendAddress}/{path}";
-//        }
+        private static string CreateUrl(string path)
+        {
+            return $"{confBackendAddress}/{path}";
+        }
 
-//        [TestMethod]
-//        public void RegisteredAsComponent_JobbrIsStarted_WebServerIsAvailable()
-//        {
-//            using (GivenARunningServerWithWebApi())
-//            {
-//                var client = new HttpClient();
-//                var result = client.GetAsync(CreateUrl("api/status")).Result;
+        [TestMethod]
+        public void RegisteredAsComponent_JobbrIsStarted_WebServerIsAvailable()
+        {
+            using (GivenARunningServerWithWebApi())
+            {
+                var client = new HttpClient();
+                var result = client.GetAsync(CreateUrl("api/status")).Result;
 
-//                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
-//            }
-//        }
+                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            }
+        }
 
-//        [TestMethod]
-//        public void RegisteredAsComponent_JobbrIsStarted_ConfigurationIsAvailable()
-//        {
-//            using (GivenARunningServerWithWebApi())
-//            {
-//                var client = new HttpClient();
-//                var result = client.GetAsync(CreateUrl("api/configuration")).Result;
+        [TestMethod]
+        public void RegisteredAsComponent_JobbrIsStarted_ConfigurationIsAvailable()
+        {
+            using (GivenARunningServerWithWebApi())
+            {
+                var client = new HttpClient();
+                var result = client.GetAsync(CreateUrl("api/configuration")).Result;
 
-//                var response = result.Content.ReadAsStringAsync().Result;
+                var response = result.Content.ReadAsStringAsync().Result;
 
-//                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
-//                Assert.IsTrue(response.Contains("{"), "There should be something looking like a serialize json object");
+                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+                Assert.IsTrue(response.Contains("{"), "There should be something looking like a serialize json object");
 
-//            }
-//        }
+            }
+        }
 
-//        [TestMethod]
-//        public void RegisteredAsComponent_JobbrIsStarted_ExceptionsDontAffectServer()
-//        {
-//            using (GivenARunningServerWithWebApi())
-//            {
-//                var client = new HttpClient();
+        [TestMethod]
+        public void RegisteredAsComponent_JobbrIsStarted_ExceptionsDontAffectServer()
+        {
+            using (GivenARunningServerWithWebApi())
+            {
+                var client = new HttpClient();
 
-//                var faultyResult = client.GetAsync(CreateUrl("api/fail")).Result;
-//                Assert.AreEqual(HttpStatusCode.InternalServerError, faultyResult.StatusCode);
+                var faultyResult = client.GetAsync(CreateUrl("api/fail")).Result;
+                Assert.AreEqual(HttpStatusCode.InternalServerError, faultyResult.StatusCode);
 
-//                var result = client.GetAsync(CreateUrl("api/status")).Result;
+                var result = client.GetAsync(CreateUrl("api/status")).Result;
 
-//                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+                Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
 
-//            }
-//        }
+            }
+        }
 
-//        [TestMethod]
-//        public void RegisteredAsComponent_JobbrIsStarted_CanLoadSomeJobs()
-//        {
-//            using (GivenARunningServerWithWebApi())
-//            {
-//                var client = new HttpClient();
+        [TestMethod]
+        public void RegisteredAsComponent_JobbrIsStarted_CanLoadSomeJobs()
+        {
+            using (GivenARunningServerWithWebApi())
+            {
+                var client = new HttpClient();
 
-//                var faultyResult = client.GetAsync(CreateUrl("api/jobs")).Result;
-//                Assert.AreEqual(HttpStatusCode.OK, faultyResult.StatusCode);
-//            }
-//        }
+                var faultyResult = client.GetAsync(CreateUrl("api/jobs")).Result;
+                Assert.AreEqual(HttpStatusCode.OK, faultyResult.StatusCode);
+            }
+        }
 
-//        [TestMethod]
-//        public void RegisteredAsComponent_WithoutConfiguration_DoesStart()
-//        {
-//            var builder = new JobbrBuilder();
-//            builder.AddWebApi();
+        [TestMethod]
+        public void RegisteredAsComponent_WithoutConfiguration_DoesStart()
+        {
+            var builder = new JobbrBuilder();
+            builder.AddWebApi();
 
-//            var server = builder.Create();
+            var server = builder.Create();
 
-//            server.Start();
+            server.Start();
 
-//            using (server)
-//            {
-//                Assert.AreEqual(JobbrState.Running, server.State, "Server should be possible to start with default configuration");
-//            }
-//        }
-//    }
-//}
+            using (server)
+            {
+                Assert.AreEqual(JobbrState.Running, server.State, "Server should be possible to start with default configuration");
+            }
+        }
+    }
+}
