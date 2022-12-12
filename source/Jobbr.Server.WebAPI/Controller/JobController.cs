8bc2ec8e80712884ed722e80ff1d2153853a287e
Mara Huminiuc <mara.huminiuc@zuehlke.com>
2022-12-12 16:15:17 +0100

    Code clean up

--- source/Jobbr.Server.WebAPI/Controller/JobController.cs
+++ source/Jobbr.Server.WebAPI/Controller/JobController.cs
@@ -29,21 +29,21 @@ namespace Jobbr.Server.WebAPI.Controller
         [Route("jobs")]
         public IActionResult AllJobs(int page = 1, int pageSize = 50, string jobTypeFilter = null, string jobUniqueNameFilter = null, string query = null, bool showDeleted = false, string sort = null)
         {
-            return Ok(this._queryService.GetJobs(page, pageSize, jobTypeFilter, jobUniqueNameFilter, query, showDeleted, sort?.Split(',')).ToPagedResult());
+            return Ok(_queryService.GetJobs(page, pageSize, jobTypeFilter, jobUniqueNameFilter, query, showDeleted, sort?.Split(',')).ToPagedResult());
         }
 
         [HttpGet]
         [Route("jobs/{jobId}")]
         public IActionResult TriggersByJob(long jobId, int page = 1, int pageSize = 200, bool showDeleted = false)
         {
-            var job = this._queryService.GetJobById(jobId);
+            var job = _queryService.GetJobById(jobId);
 
             if (job == null)
             {
                 return NotFound();
             }
 
-            var pagedResult = this._queryService.GetTriggersByJobId(jobId, page, pageSize, showDeleted);
+            var pagedResult = _queryService.GetTriggersByJobId(jobId, page, pageSize, showDeleted);
 
             var jobDto = job.ToDto();
             jobDto.Trigger = pagedResult.Items.Select(t => TriggerMapper.ConvertToDto(t as dynamic)).Cast<JobTriggerDtoBase>().ToList();
@@ -62,7 +62,7 @@ namespace Jobbr.Server.WebAPI.Controller
                 return NotFound();
             }
 
-            var existingJob = this._queryService.GetJobByUniqueName(identifier);
+            var existingJob = _queryService.GetJobByUniqueName(identifier);
 
             if (existingJob != null)
             {
@@ -70,7 +70,7 @@ namespace Jobbr.Server.WebAPI.Controller
             }
 
             var job = new Job { UniqueName = dto.UniqueName, Title = dto.Title, Type = dto.Type, Parameters = dto.Parameters != null ? JsonConvert.SerializeObject(dto.Parameters) : null, };
-            this._jobManagementService.AddJob(job);
+            _jobManagementService.AddJob(job);
 
             return Created("jobs/" + job.Id, job.ToDto());
         }
@@ -79,7 +79,7 @@ namespace Jobbr.Server.WebAPI.Controller
         [Route("jobs/{jobId:long}")]
         public IActionResult UpdateJob(long jobId, [FromBody] JobDto dto)
         {
-            var existingJob = this._queryService.GetJobById(jobId);
+            var existingJob = _queryService.GetJobById(jobId);
 
             if (existingJob == null)
             {
@@ -92,21 +92,21 @@ namespace Jobbr.Server.WebAPI.Controller
         [Route("jobs/{jobId:int}/runs")]
         public IActionResult GetJobRunsForJobById(int jobId, int page = 1, int pageSize = 50, bool showDeleted = false, string sort = null)
         {
-            return Ok(this._queryService.GetJobRunsByJobId(jobId, page, pageSize, showDeleted, sort?.Split(',')).ToPagedResult());
+            return Ok(_queryService.GetJobRunsByJobId(jobId, page, pageSize, showDeleted, sort?.Split(',')).ToPagedResult());
         }
 
         [HttpGet]
         [Route("jobs/{uniqueName}/runs")]
         public IActionResult GetJobRunsForJobByUniqueName(string uniqueName, int page = 1, int pageSize = 50, bool showDeleted = false, string sort = null)
         {
-            var job = this._queryService.GetJobByUniqueName(uniqueName);
+            var job = _queryService.GetJobByUniqueName(uniqueName);
 
             if (job == null)
             {
                 return NotFound();
             }
 
-            var jobRuns = this._queryService.GetJobRunsByJobId((int)job.Id, page, pageSize, showDeleted, sort?.Split(','));
+            var jobRuns = _queryService.GetJobRunsByJobId((int)job.Id, page, pageSize, showDeleted, sort?.Split(','));
 
             return Ok(jobRuns.ToPagedResult());
         }
