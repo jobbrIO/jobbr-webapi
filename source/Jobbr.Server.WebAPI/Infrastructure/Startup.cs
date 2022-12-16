3bb5fa30d2d68102649254993f9337c81ba3c896
Mara Huminiuc <mara.huminiuc@zuehlke.com>
2022-12-16 17:54:49 +0100

    Use WebHost hosting

---
+++ source/Jobbr.Server.WebAPI/Infrastructure/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Jobbr.Server.WebAPI.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Jobbr.Server.WebAPI.Infrastructure
+{
+    public class Startup
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly JobbrWebApiConfiguration _configuration;
+
+        public Startup(ILoggerFactory loggerFactory, JobbrWebApiConfiguration configuration)
+        {
+            _loggerFactory = loggerFactory;
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(o =>
+            {
+                o.Filters.Add(new ResponseCacheAttribute { NoStore = true, Location = ResponseCacheLocation.None });
+            });
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNamingPolicy = DefaultJsonOptions.Options.PropertyNamingPolicy;
+                    options.JsonSerializerOptions.PropertyNameCaseInsensitive = DefaultJsonOptions.Options.PropertyNameCaseInsensitive;
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = DefaultJsonOptions.Options.DefaultIgnoreCondition;
+                    options.JsonSerializerOptions.Converters.Add(new JsonTypeConverter<JobTriggerDtoBase>(_loggerFactory, "TriggerType", JobTriggerTypeResolver));
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", config => config
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UsePathBase(new Uri(_configuration.BackendAddress).AbsolutePath);
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseCors();
+        }
+
+        private Type JobTriggerTypeResolver(List<Type> types, string typeValue)
+        {
+            if (typeValue.ToLowerInvariant() == RecurringTriggerDto.Type.ToLowerInvariant())
+            {
+                return typeof(RecurringTriggerDto);
+            }
+
+            if (typeValue.ToLowerInvariant() == ScheduledTriggerDto.Type.ToLowerInvariant())
+            {
+                return typeof(ScheduledTriggerDto);
+            }
+
+            if (typeValue.ToLowerInvariant() == InstantTriggerDto.Type.ToLowerInvariant())
+            {
+                return typeof(InstantTriggerDto);
+            }
+
+            return null;
+        }
+    }
+}
--- source/Jobbr.Server.WebAPI/Infrastructure/WebHost.cs
+++ source/Jobbr.Server.WebAPI/Infrastructure/WebHost.cs
@@ -1,14 +1,9 @@
 using Jobbr.ComponentModel.Registration;
-using Jobbr.Server.WebAPI.Model;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SimpleInjector;
 using System;
-using System.Collections.Generic;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Jobbr.Server.WebAPI.Infrastructure
@@ -18,9 +13,8 @@ namespace Jobbr.Server.WebAPI.Infrastructure
     /// </summary>
     public class WebHost : IJobbrComponent
     {
-        private WebApplication _webApp;
+        private IWebHost _webHost;
         private readonly ILogger _logger;
-        private readonly ILoggerFactory _loggerFactory;
         private readonly InstanceProducer[] _serviceCollection;
         private readonly JobbrWebApiConfiguration _configuration;
 
@@ -32,7 +26,6 @@ namespace Jobbr.Server.WebAPI.Infrastructure
         /// <param name="configuration">Web API configuration.</param>
         public WebHost(ILoggerFactory loggerFactory, Container container, JobbrWebApiConfiguration configuration)
         {
-            _loggerFactory = loggerFactory;
             _logger = loggerFactory.CreateLogger<WebHost>();
             _serviceCollection = container.GetCurrentRegistrations();
             _configuration = configuration;
@@ -43,69 +36,49 @@ namespace Jobbr.Server.WebAPI.Infrastructure
         /// </summary>
         public void Start()
         {
-            if (_webApp != null)
+            if (_webHost != null)
             {
                 throw new InvalidOperationException("The server has already been started.");
             }
 
             AssertBackendAddressIsValid();
 
-            var builder = WebApplication.CreateBuilder();
-
-            foreach (var instanceProducer in _serviceCollection)
-            {
-                builder.Services.Add(new ServiceDescriptor(instanceProducer.ServiceType, instanceProducer.GetInstance()));
-            }
-
-            builder.Services.AddMvc(o =>
-            {
-                o.Filters.Add(new ResponseCacheAttribute { NoStore = true, Location = ResponseCacheLocation.None });
-            });
-
-            builder.Services
-                .AddControllers()
-                .AddJsonOptions(options =>
+            _webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls(new Uri(_configuration.BackendAddress).GetLeftPart(UriPartial.Authority))
+                .ConfigureServices(services =>
                 {
-                    options.JsonSerializerOptions.PropertyNamingPolicy = DefaultJsonOptions.Options.PropertyNamingPolicy;
-                    options.JsonSerializerOptions.PropertyNameCaseInsensitive = DefaultJsonOptions.Options.PropertyNameCaseInsensitive;
-                    options.JsonSerializerOptions.DefaultIgnoreCondition = DefaultJsonOptions.Options.DefaultIgnoreCondition;
-                    options.JsonSerializerOptions.Converters.Add(new JsonTypeConverter<JobTriggerDtoBase>(_loggerFactory, "TriggerType", JobTriggerTypeResolver));
+                    foreach (var instanceProducer in _serviceCollection)
+                    {
+                        services.Add(new ServiceDescriptor(instanceProducer.ServiceType, instanceProducer.GetInstance()));
+                    }
                 })
-                .AddApplicationPart(typeof(WebHost).Assembly);
+                .UseStartup<Startup>()
+                .Build();
 
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy", config => config
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
-            });
-
-            _webApp = builder.Build();
-
-            _webApp.MapControllers();
-
-            var backendAddressUri = new Uri(_configuration.BackendAddress);
-            _webApp.UsePathBase(backendAddressUri.AbsolutePath);
-            _webApp.Urls.Add(backendAddressUri.GetLeftPart(UriPartial.Authority));
-            _webApp.UseRouting();
-
-            _webApp.UseCors();
-
-            Task.FromResult(_webApp.StartAsync());
+            _webHost.Start();
 
             _logger.LogInformation("Started web host for WebAPI at '{backendAddress}'.", _configuration.BackendAddress);
         }
 
         public void Stop()
         {
-            Task.FromResult(_webApp.StopAsync());
+            Task.FromResult(_webHost.StopAsync());
         }
 
         public void Dispose()
         {
-            Task.FromResult(_webApp.StopAsync());
-            Task.FromResult(_webApp.DisposeAsync());
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Task.FromResult(_webHost.StopAsync());
+                _webHost?.Dispose();
+            }
         }
 
         private void AssertBackendAddressIsValid()
@@ -120,26 +93,6 @@ namespace Jobbr.Server.WebAPI.Infrastructure
             {
                 throw new FormatException("No valid UriScheme was given. Please provide a scheme like http(s)");
             }
-        }
-
-        private Type JobTriggerTypeResolver(List<Type> types, string typeValue)
-        {
-            if (typeValue.ToLowerInvariant() == RecurringTriggerDto.Type.ToLowerInvariant())
-            {
-                return typeof(RecurringTriggerDto);
-            }
-
-            if (typeValue.ToLowerInvariant() == ScheduledTriggerDto.Type.ToLowerInvariant())
-            {
-                return typeof(ScheduledTriggerDto);
-            }
-
-            if (typeValue.ToLowerInvariant() == InstantTriggerDto.Type.ToLowerInvariant())
-            {
-                return typeof(InstantTriggerDto);
-            }
-
-            return null;
         }
     }
 }
